23c43fce2d20c39191db98bc8d043d08635f40c5
Alejo Kannemann <alejokannemann@gmail.com>
2023-05-25 17:40:52 -0300

    formato

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import TituloH3 from './practicas/C2/componentes/TituloH3.jsx'
 import ChildrenLista from './practicas/C2/componentes/ChildrenLista.jsx'
+import Cards from './practicas/C4/componentes/Cards.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './practicas/C4/App.jsx'
 import './index.css'
-import Cards from './practicas/C4/componentes/Cards.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <App />
-    {/* Descomentar y cambiar el import App en el path practicas/C4/App.jsx
+    
+    {/*
     <Cards title="titulo de la card" footer="footer de la card">
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed aut fugiat, voluptatem est in ducimus repellat dolorem consequatur optio tempore. Accusantium praesentium facilis vitae maxime inventore asperiores voluptates perferendis accusamus.
