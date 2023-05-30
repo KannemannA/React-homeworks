6832eee718dff21ef0cbcec88ef444ee817b4068
Alejo Kannemann <alejokannemann@gmail.com>
2023-05-30 20:29:59 -0300

    Ten piedad React

--- src/practicas/C9/App.jsx
+++ src/practicas/C9/App.jsx
@@ -1,30 +1,38 @@
 import { useState } from "react";
 import "../../App.css";
 
+let id=0
 function App() {
-  const [dataForm, setDataForm] = useState([])
+  const [dataForm, setDataForm] = useState()
+  const [data, setData] = useState({})
+  const [error,setError] = useState("")
   const formData=(e)=>{
     e.preventDefault()
-    let obj={
-      nombre: e.target[0].value,
-      edad: e.target[1].value
+    let daitem = [data]
+    if(dataForm){
+      setDataForm(...dataForm,daitem)
+      return
     }
-    setDataForm(...dataForm, [obj])
+    setDataForm(daitem)
+  }
+  const handleInputValues=(e)=>{
+    setData({
+      ...data,
+      [e.target.id]: e.target.value
+    })
   }
   return (
     <>
     <form onSubmit={formData}>
       <label htmlFor="nombre">Nombre:</label>
-      <input type="text" id="nombre" required />
+      <input type="text" id="nombre" required onBlur={handleInputValues}/>
       <label htmlFor="edad">Edad:</label>
-      <input type="number" id="edad" required/>
+      <input type="number" id="edad" required onBlur={handleInputValues}/>
       <button type="submit" > enviar</button>
     </form>
     <h2>registro:</h2>
     <ul>
-      {dataForm.map((result)=>{
-        <li>Nombre: {result}, edad: {result}.</li>
-      })}
+      {/* {dataForm.map((item)=><li key={item.nombre+id++}>Nombre: {item.nombre} | Edad: {item.edad}</li>)} */}
     </ul>
     </>
   )
