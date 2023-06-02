77842f40d89bbb96dad96a24aa5cef1bcd7545d6
Alejo Kannemann <alejokannemann@gmail.com>
2023-06-02 15:13:57 -0300

    clase 9 terminado

--- src/practicas/C9/App.jsx
+++ src/practicas/C9/App.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import "../../App.css";
 
+
 let id=0
 function App() {
-  const [dataForm, setDataForm] = useState()
+  const [dataForm, setDataForm] = useState([])
   const [data, setData] = useState({})
-  const [error,setError] = useState("")
+  const [error,setError] = useState('')
   const formData=(e)=>{
     e.preventDefault()
-    let daitem = [data]
-    if(dataForm){
-      setDataForm(...dataForm,daitem)
+    setError('')
+    if(data.nombre.length<=3 || data.numero<=0){
+      setError("ingrese un nombre con un minimo de 4 caracteres e ingrese un numero mayor a 0")
       return
     }
-    setDataForm(daitem)
+    setDataForm([...dataForm, data])
   }
   const handleInputValues=(e)=>{
     setData({
@@ -26,13 +27,15 @@ function App() {
     <form onSubmit={formData}>
       <label htmlFor="nombre">Nombre:</label>
       <input type="text" id="nombre" required onBlur={handleInputValues}/>
-      <label htmlFor="edad">Edad:</label>
-      <input type="number" id="edad" required onBlur={handleInputValues}/>
+      <label htmlFor="edad">Numero favorito:</label>
+      <input type="number" id="numero" required onBlur={handleInputValues}/>
       <button type="submit" > enviar</button>
+      <br />
+      {error?<p style={{color: "red"}}>{error}</p>:undefined}
     </form>
     <h2>registro:</h2>
     <ul>
-      {/* {dataForm.map((item)=><li key={item.nombre+id++}>Nombre: {item.nombre} | Edad: {item.edad}</li>)} */}
+      {dataForm.map((item)=><li key={item.nombre+id++}>Nombre: {item.nombre} <br /> tu numero favorito es {item.numero}</li>)}
     </ul>
     </>
   )
